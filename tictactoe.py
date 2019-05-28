5ddb01dd555cd4b999ed83271cf00d5ee3604a99
Ryan Chartier <redrecrm@gmail.com>
2019-05-27 23:08:34 -0600

    Modified tictactoe

--- tictactoe.py
+++ tictactoe.py
@@ -3,7 +3,7 @@ import udebs
 import json
 
 class TicTacToe(udebs.State):
-    endgames = {"o": -1, "t": 0, "x": 1, "": None}
+    algorithm = "bruteForce"
     
     def legalMoves(self, state):
         """Create a list of legal moves in current state space."""
@@ -14,65 +14,70 @@ class TicTacToe(udebs.State):
                 yield player, loc, "placement"
 
     def endState(self, state):
-        return self.endgames[state.getStat("player", "leaf")]
+        def rows(gameMap):
+            """Iterate over possible win conditions in game map."""
+            size = len(gameMap)
+
+            for i in range(size):
+                yield gameMap[i]
+                yield [j[i] for j in gameMap]
+
+            yield [gameMap[i][i] for i in range(size)]
+            yield [gameMap[size - 1 - i][i] for i in range(size)]
+
+        # Check for a win
+        tie = True
+        for i in rows(state.map["map"].map):
+            value = set(i)
+            if "empty" in value:
+                tie = False
+            elif len(value) == 1:
+                if i[0] == "x":
+                    return 1
+                elif i[0] == "o":
+                    return -1
+
+        if tie:
+            return 0
     
     def name(self):
-        player, (x, y, _), _ = self.entry
-        return player[0] + "_" + str(x) + "_" + str(y)
+        if self.entry is not None:
+            player, (x, y, _), _ = self.entry
+            return player[0] + "_" + str(x) + "_" + str(y)
 
-# Setup Udebs
-def endgame(state):
-    def rows(gameMap):
-        """Iterate over possible win conditions in game map."""
-        size = len(gameMap)
+        return "root"
 
-        for i in range(size):
-            yield gameMap[i]
-            yield [j[i] for j in gameMap]
+def initialMoves(game, moves):
+    player = "xPlayer"
+    for move in moves:
+        game.controlMove(player, move, "placement")
+        player = ("xPlayer" if player == "oPlayer" else "oPlayer")
 
-        yield [gameMap[i][i] for i in range(size)]
-        yield [gameMap[size - 1 - i][i] for i in range(size)]
+    return len(moves) % 2
 
-    # Check for a win
-    tie = True
-    for i in rows(state.map["map"].map):
-        value = set(i)
-        if "empty" in value:
-            tie = False
-        elif len(value) == 1:
-            return i[0]
-
-    if tie:
-        return "t"
+def checkTree(value):
+    if isinstance(value, dict):
+        return {i:checkTree(v) for i,v in value.items()}
+    else:
+        return "{}-{}".format(value.value, value.turns)
 
 if __name__ == "__main__":
-    module = {"ENDGAME": {
-        "f": "endgame",
-        "args": ["self"],
-    }}
-    udebs.importModule(module, {"endgame": endgame})
     game = udebs.battleStart("xml/tictactoe.xml")
 
     # Setup state
-    moves = [
-    ]
-
-    # Play Startup Moves.
-    player = "xPlayer"
-    for i in moves:
-        game.controlMove(player, i, "placement")
-        player = ("xPlayer" if player == "oPlayer" else "oPlayer")
+    offset = initialMoves(game, [
+    ])
 
     # Print starting state.
     for row in game.map["map"].map:
         print(*(i if i != "empty" else "-" for i in row))
 
     # Create state analysis engine
-    analysis = TicTacToe(game, "bruteForce")
-    final = analysis.result(len(moves) % 2)
-    print("final", final.value)
+    analysis = TicTacToe(game)
+    final = analysis.result(offset)
+    print("final", final.value, final.turns)
     print("states checked", len(analysis.storage))
     
     # Save tree
     with open("results/tictactoe.json", "w+") as f:
-        json.dump(analysis.tree(), f)
+        json.dump(checkTree(analysis.tree()), f)
