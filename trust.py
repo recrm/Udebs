e270d7b27b49aea9f800e1bc5504c7bfcd9cac8b
Ryan Chartier <redrecrm@gmail.com>
2019-09-26 19:17:22 -0600

    Move trust to a script

--- trust.py
+++ trust.py
@@ -1,59 +1,147 @@
 #!/usr/bin/env python3
 import udebs
 import itertools
+import random
+import itertools
+from collections import Counter
 
-if __name__ == "__main__":
+class Random(udebs.Player):
+    def __call__(self, state):
+        return random.choice(["cheat", "cooperate"])
 
-    options = ["cheat", "cooperate"]
+class Human(udebs.Player):
+    def __call__(self, state):
+        x = input("what do you play? ")
+        while x not in ["cheat", "cooperate"]:
+            x = input("try again")
 
-    udebs.lookup("LOOKUP", {
-        "cooperate": {
-            "cooperate": 1,
-            "cheat": -1,
-        },
-        "cheat": {
-            "cooperate": 3,
-        },
-        "timer": 10,
-    })
+        return x
 
-    players = [{
-        "f": udebs.copyPlayer,
-        "kwargs": {"default": "cooperate"},
-        "name": "copy",
-        "score": 0,
-    }, {
-        "f": udebs.constantPlayer,
-        "kwargs": {"constant": "cooperate"},
-        "name": "always cooperate",
-        "score": 0
-    }, {
-        "f": udebs.constantPlayer,
-        "kwargs": {"constant": "cheat"},
-        "name": "always cheat",
-        "score": 0,
-    }, {
-        "f": udebs.randomPlayer,
-        "kwargs": {},
-        "name": "random",
-        "score": 0,
-    }, {
-        "f": udebs.grudgerPlayer,
-        "kwargs": {"default": "cooperate", "trigger": "cheat"},
-        "name": "grudger",
-        "score": 0,
-    }]
+class AlwaysTrust(udebs.Player):
+    def __call__(self, state):
+        return "cooperate"
 
-    for x, y in itertools.combinations_with_replacement(players, 2):
-        x["f"]("CHOICE_C", options, **x["kwargs"])
-        y["f"]("CHOICE_H", options, **y["kwargs"])
+class AlwaysCheat(udebs.Player):
+    def __call__(self, state):
+        return "cheat"
 
-        game = udebs.battleStart("xml/trust.xml")
-        for i in game.gameLoop():
+class Grudger(udebs.Player):
+    def __call__(self, state):
+        if state.getStat("computer", "score") != state.getStat("human", "score"):
+            self.grudged = True
+
+        if getattr(self, "grudged", False):
+            return "cheat"
+
+        return "cooperate"
+
+class Forgiver(udebs.Player):
+    def __init__(self, *args, **kwargs):
+        self.previous = "cooperate"
+        self.score = 0
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, state):
+        score = state.getStat("computer", "score") + state.getStat("human", "score")
+        if score - self.score == 2:
+            self.previous = "cheat"
+        if score - self.score == 6:
+            self.previous = "cooperate"
+        if score - self.score == 5:
+            if self.previous == "cheat":
+                self.previous = "cooperate"
+            else:
+                self.previous = "cheat"
+
+        self.score = score
+        return self.previous
+
+class CheatFirst(udebs.Player):
+    def __call__(self, state):
+        if getattr(self, "cheated", False):
+            return "cooperate"
+
+        self.cheated = True
+        return "cheat"
+
+class TrustFirst(udebs.Player):
+    def __call__(self, state):
+        if getattr(self, "cheated", False):
+            return "cheat"
+
+        self.cheated = True
+        return "cooperate"
+
+udebs.lookup("LOOKUP", {
+    "cooperate": {
+        "cooperate": 2,
+        "cheat": -1,
+    },
+    "cheat": {
+        "cooperate": 3,
+        "cheat": 0,
+    },
+    "timer": 10,
+})
+
+def meta(players, spaces, strategies):
+    while True:
+        results = tourney(players, spaces).most_common()
+
+        windex, winner = results[0][0].split("_")
+        lindex, loser = results[-1][0].split("_")
+        runnerx, runner = results[-2][0].split("_")
+
+        explore = random.choice(strategies)
+
+        yield results
+
+        players[int(lindex)] = players[int(windex)]
+        players[int(runnerx)] = random.choice(strategies)
+
+def tourney(players, spaces):
+    scores = Counter()
+    for args in itertools.combinations(enumerate(players), len(spaces)):
+        for (index, player), role in zip(args, spaces):
+            player(role)
+
+        main_map = udebs.battleStart("xml/trust.xml")
+        for i in main_map.gameLoop():
             pass
 
-        x["score"] += game.getStat("computer", "score")
-        y["score"] += game.getStat("human", "score")
+        for (index, player), role in zip(args, ["human", "computer"]):
+            scores[str(index) + "_" + player.__name__] += main_map.getStat(role, "score")
 
-    for i in players:
-        print(i["name"], i["score"])
+    return scores
+
+if __name__ == "__main__":
+    scores = Counter()
+
+    strategies = [Random, AlwaysTrust, AlwaysCheat, Grudger, CheatFirst, TrustFirst, Forgiver]
+
+    players = [
+        Random,
+        Random,
+        Random,
+        AlwaysTrust,
+        AlwaysTrust,
+        AlwaysTrust,
+        AlwaysCheat,
+        AlwaysCheat,
+        AlwaysCheat,
+        Grudger,
+        Grudger,
+        Grudger,
+        Forgiver,
+        Forgiver,
+        Forgiver,
+    ]
+
+    spaces = ["HUMAN", "COMPUTER"]
+
+    for results in meta(players, spaces, strategies):
+        for x, y in results:
+            print(x, y)
+        test = input()
+        if test == "n":
+            break
